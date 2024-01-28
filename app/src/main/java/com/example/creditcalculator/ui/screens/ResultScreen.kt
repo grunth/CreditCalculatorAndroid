fe025b1b89552667de6af7146bd26a96bac349fc
grigory osipov <vn56@bk.ru>
2024-01-28 19:36:27 +0100

    Добавил таблицу для вывода результатов

--- app/src/main/java/com/example/creditcalculator/ui/screens/ResultScreen.kt
+++ app/src/main/java/com/example/creditcalculator/ui/screens/ResultScreen.kt
@@ -1,30 +1,91 @@
 package com.example.creditcalculator.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.creditcalculator.model.CreditDataViewModel
 
 @Composable
 fun ResultScreen(creditViewModel: CreditDataViewModel) {
-    val creditData = creditViewModel.creditData
-    println(creditData)
+    var creditData = creditViewModel.creditData
     val resultText by remember { mutableStateOf("Результат расчета") }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = resultText, modifier = Modifier.align(CenterHorizontally))
+
+    val tableData = (1..100).mapIndexed { index, item ->
+        index to "Item $index"
     }
+    LazyColumn(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Here is the header
+        item {
+            Row(Modifier.background(Color.Gray)) {
+                TableCell(text = "Сумма кредита", weight = .25f)
+                TableCell(text = creditData.loanAmount, weight = .25f)
+                TableCell(text = "Процентная ставка", weight = .25f)
+                TableCell(text = creditData.interestRate, weight = .25f)
+            }
+            Row(Modifier.background(Color.Gray)) {
+                TableCell(text = "Срок погашения", weight = .25f)
+                TableCell(text = creditData.loanTerm, weight = .25f)
+                TableCell(text = "Способ погашения", weight = .25f)
+                TableCell(text = creditData.repaymentMethod, weight = .25f)
+            }
+            Row(Modifier.background(Color.LightGray)) {
+                TableCell(text = "Месяц", weight = .1f)
+                TableCell(text = "Непогашенная сумма основного долга", weight = .225f)
+                TableCell(text = "Сумма месячного погашенного взноса", weight = .225f)
+                TableCell(text = "Процентные платежи", weight = .225f)
+                TableCell(text = "Месячная выплата основного долга", weight = .225f)
+            }
+        }
+
+        items(tableData) {
+            val (id, text) = it
+            Row(Modifier.fillMaxWidth()) {
+                TableCell(text = id.toString(), weight = .1f)
+                TableCell(text = "test1", weight = .2f)
+                TableCell(text = "test2", weight = .2f)
+                TableCell(text = "test3", weight = .2f)
+                TableCell(text = "test4", weight = .2f)
+            }
+        }
+
+        item {
+            Row(Modifier.background(Color.DarkGray)) {
+                TableCell(text = "ИТОГО", weight = .1f)
+                TableCell(text = "сум", weight = .225f)
+                TableCell(text = "сум", weight = .225f)
+                TableCell(text = "сум", weight = .225f)
+                TableCell(text = "сум", weight = .225f)
+            }
+        }
+    }
+}
+
+@Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float
+) {
+    Text(
+        text = text,
+        Modifier
+            .weight(weight)
+            .padding(8.dp)
+    )
 }
